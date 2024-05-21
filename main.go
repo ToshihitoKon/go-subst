52f4320f1664311adf239a7bba9ec14bbe3b49a7
ToshihitoKon <kon.toshihito@donuts.ne.jp>
2024-05-21 13:29:43 +0900

    bytes

--- main.go
+++ main.go
@@ -8,8 +8,8 @@ import (
 )
 
 type CLI struct {
-	input   string
-	result  string
+	input   []byte
+	result  []byte
 	output  *bufio.Writer
 	context context.Context
 }
@@ -38,18 +38,27 @@ func (c *CLI) prepare() error {
 	if err := c.scanStdin(); err != nil {
 		return fmt.Errorf("scanStdin: %w", err)
 	}
+
 	return nil
 }
 
 func (c *CLI) process() error {
+<<<<<<< Updated upstream
 	// TODO: impl template process
 	result := c.input
+=======
+	result, err := goconfig.ReadWithEnvBytes(c.input)
+	if err != nil {
+		return fmt.Errorf("goconfig.ReadWithEnvBytes failed: %w", err)
+	}
+
+>>>>>>> Stashed changes
 	c.result = result
 	return nil
 }
 
 func (c *CLI) flush() error {
-	if _, err := c.output.WriteString(c.result); err != nil {
+	if _, err := c.output.Write(c.result); err != nil {
 		return fmt.Errorf("WriteString failed: %w", err)
 	}
 	if err := c.output.Flush(); err != nil {
@@ -60,16 +69,27 @@ func (c *CLI) flush() error {
 }
 
 func (c *CLI) optParse() error {
-	// TODO: impl --output
+	// TODO: impl options input,output
 	c.output = bufio.NewWriter(os.Stdout)
 	return nil
 }
 
 func (c *CLI) scanStdin() error {
 	scanner := bufio.NewScanner(os.Stdin)
-	if ok := scanner.Scan(); !ok {
-		return fmt.Errorf("scanner.Scan failed: %w", scanner.Err())
+	scanner.Split(func(data []byte, atEOF bool) (int, []byte, error) {
+		if atEOF && len(data) == 0 {
+			return 0, nil, nil
+		}
+		return len(data), data, nil
+	})
+
+	for scanner.Scan() {
+		c.input = append(c.input, scanner.Bytes()...)
 	}
-	c.input = scanner.Text()
+
+	if err := scanner.Err(); err != nil {
+		return fmt.Errorf("scanner.Scan failed: %w", err)
+	}
+
 	return nil
 }
